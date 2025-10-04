4e9277382e929f51f231d4fdbc3bb8d087643592
Pushkar Kumar Purve <kpushkar658@gmail.com>
2025-10-04 22:57:09 +0530

    updated files added

---
+++ script.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector('.nav');
+const open = document.querySelector('.open');
+const close = document.querySelector('.close');
+open.addEventListener('click',()=>{
+    menu.classList.add("show");
+    close.style.display = 'inline-block';
+    open.style.display = 'none';
+})
+close.addEventListener('click', ()=>{
+    menu.classList.remove("show");
+    open.style.display = 'inline-block';
+    close.style.display = 'none';
+})
